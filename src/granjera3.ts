5fd75bb6d284760b92fdce6d488a0fc70b70c403
CARMENSOSA19 <104264514+CARMENSOSA19@users.noreply.github.com>
2022-04-23 12:29:13 -0300

    Initial commit

---
+++ src/granjera3.ts
@@ -0,0 +1,8 @@
+console.log("giro hacia la izquierda");
+console.log("avanza");
+console.log("giro hacia la derecha");
+console.log("avanza");
+console.log("avanza");
+console.log("giro hacia la derecha");
+console.log("avanza");
+console.log("elimina 1");
